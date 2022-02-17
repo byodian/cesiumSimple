4fe6f66829ce2982e5d5346d3af55c7ed22504d1
baiyongjian <yongjian.bai@nriat.com>
2022-02-17 18:06:19 +0800

    feat: 更新 webpack

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import * as Cesium from 'cesium'
-import 'cesium/Build/Cesium/Widgets/widgets.css'
+// import 'cesium/Widget/widgets.css'
+import { 
+  Viewer, 
+  Ion, 
+  Rectangle,
+  IonWorldImageryStyle,
+  IonImageryProvider,
+  ArcGisMapServerImageryProvider,
+  defined,
+  when,
+  ScreenSpaceEventType,
+  ScreenSpaceEventHandler,
+  SingleTileImageryProvider,
+  createWorldTerrain,
+  createWorldImagery,
+  SceneMode
+} from 'cesium'
 
-console.log(process.env.CESIUM_TOKENID)
-Cesium.Ion.defaultAccessToken = process.env.CESIUM_TOKENID
+Ion.defaultAccessToken = process.env.CESIUM_TOKENID
 
-const viewer = new Cesium.Viewer('cesium-container', {
-  terrainProvider: Cesium.createWorldTerrain({
+const viewer = new Viewer('cesium-container', {
+  terrainProvider: createWorldTerrain({
     requestWaterMask: true,
     requestVertexNormals: true
   }),
-  imageryProvider : Cesium.createWorldImagery({
-    style : Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS
+  imageryProvider : createWorldImagery({
+    style : IonWorldImageryStyle.AERIAL_WITH_LABELS
   }),
   timeline: false,
   baseLayerPicker : false,
-  sceneMode: Cesium.SceneMode.SCENE2D,
+  sceneMode: SceneMode.SCENE2D,
   fullscreenButton: false,
   animation: false,
   homeButton: false,
@@ -22,12 +36,6 @@ const viewer = new Cesium.Viewer('cesium-container', {
   infoBox: false,
 }); 
     
-const options = {
-  style: 'normal',
-  crs: 'BD09'
-}
-
-console.log(options)
 
 // remove the credit logo of the Cesium map
 viewer.cesiumWidget.creditContainer.style.display = 'none'
@@ -39,26 +47,26 @@ viewer.cesiumWidget.creditContainer.style.display = 'none'
 const layers = viewer.scene.imageryLayers
 
 const blackMarble = layers.addImageryProvider(
-  new Cesium.IonImageryProvider({ assetId: 3812 })
+  new IonImageryProvider({ assetId: 3812 })
 );
 blackMarble.alpha = 0.5
 blackMarble.brightness = 2 
 
 // add a single tile imagery provider  
 layers.addImageryProvider(
-  new Cesium.SingleTileImageryProvider({
-    url : './images/images.png',
-    rectangle : Cesium.Rectangle.fromDegrees(-75.0, 28.0, -67.0, 29.75)
+  new SingleTileImageryProvider({
+    url: './images/images.png',
+    rectangle: Rectangle.fromDegrees(-75.0, 28.0, -67.0, 29.75)
   })); 
 
-const mapServerProvider = new Cesium.ArcGisMapServerImageryProvider({
+const mapServerProvider = new ArcGisMapServerImageryProvider({
   url: 'https://map.geoq.cn/arcgis/rest/services/ChinaOnlineStreetGray/MapServer'
 })
 
 layers.addImageryProvider(mapServerProvider)
   
 // get location of a point
-const handler = new Cesium.ScreenSpaceEventHandler(viewer.canvas)
+const handler = new ScreenSpaceEventHandler(viewer.canvas)
 handler.setInputAction(function(event) {
   // mapServerProvider.pickFeatures(event.position).then((fearures) => {
   //   console.log(fearures)
@@ -67,10 +75,10 @@ handler.setInputAction(function(event) {
   const pickRay = viewer.camera.getPickRay(event.position)
   const featurePromise = viewer.imageryLayers.pickImageryLayerFeatures(pickRay, viewer.scene)
 
-  if (!Cesium.defined(featurePromise)) {
+  if (!defined(featurePromise)) {
     console.log('No features picked.')
   } else {
-    Cesium.when(featurePromise, function(features) {
+    when(featurePromise, function(features) {
       console.log('Number of features picked: ' + features.length)
       console.log(features)
       if (features.length > 0) {
@@ -84,6 +92,6 @@ handler.setInputAction(function(event) {
   // if (Cesium.defined(pickedPosition)) {
   //   console.log(pickedPosition)
   // }
-}, Cesium.ScreenSpaceEventType.LEFT_CLICK)
+}, ScreenSpaceEventType.LEFT_CLICK)
 
 console.log(viewer)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+const cesiumSource = 'node_modules/cesium/Source'
+const cesiumWorkers = '../Build/Cesium/Workers'
 
 module.exports = {
   entry: './src/index.js',
@@ -9,6 +15,10 @@ module.exports = {
     clean: true
   },
   devtool: 'inline-source-map',
+  amd: {
+    // Enable webpack-friendly use of require in cesium
+    toUrlUndefined: true
+  },
   module: {
     rules: [
       {
@@ -21,13 +31,32 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      cesium: path.resolve(__dirname, cesiumSource)
+    },
+    mainFiles: ['module', 'main', 'Cesium'],
+  },
   devServer: {
     static: './dist'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    // copy Cesium Assets, Widgets, and Workers to a static directory
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
+        { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
+        { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
+      ]
+    }),
+    new webpack.DefinePlugin({
+      // Define relative base path in cesium for loading assets
+      CESIUM_BASE_URL: JSON.stringify('')
+    }),
+    new Dotenv()
   ],
   mode: 'development',
 }
